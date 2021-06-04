72dc55b8e5ce057151522d4a53b32f4c9b682c6f
Zhiming Ni <1078830088@qq.com>
2021-06-04 16:54:51 +0800

    create vue project

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+'use strict'
+const path = require('path')
+const defaultSettings = require('./src/settings')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+// page title
+const name = defaultSettings.title || 'Vue Family Learning'
+// dev port
+const port = process.env.port || process.env.npm_config_port || 9898
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'build',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    //before: require('./mock/mock-server.js')
+  }
+}
