6260a29d8d7e9b1c8161eb10057acc1a98c8b13b
Zhiming Ni <1078830088@qq.com>
2021-06-04 16:55:14 +0800

    create vue project

---
+++ src/settings.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'Vue Family Learning',
+
+  /**
+   * @type {boolean}
+   * @description Whether show the settings right-panel
+   */
+  showSettings: true,
+
+  /**
+   * @type {boolean}
+   * @description Whether need tagsView
+   */
+  tagsView: true,
+
+  /**
+   * @type {string | array} 'production' | ['production', 'development']
+   * @description Need show err logs component
+   * The default is only used in the production env
+   * If you want to also use it in dev, you can pass ['production', 'development']
+   */
+  errorLog: 'production'
+}
