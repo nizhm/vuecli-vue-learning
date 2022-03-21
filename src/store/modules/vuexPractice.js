b5ff24103fe5c89f41ca413bb83b42d83f846ce4
nzhming <1078830088@qq.com>
2022-03-21 19:24:12 +0800

    learn mutation/action/getter

--- src/store/modules/vuexPractice.js
+++ src/store/modules/vuexPractice.js
@@ -1,3 +1,5 @@
+import el from 'element-ui/src/locale/lang/el'
+
 const state = {
   globalName: 'nizhm',
   globalAge: '24',
@@ -8,15 +10,61 @@ const state = {
   globalWorkProvince: 'HuNan'
 }
 
+const getters = {
+  heightByMeter: state => {
+    if (!state.globalHeight) {
+      return state.globalHeight
+    } else {
+      return parseInt(state.globalHeight) / 100
+    }
+  },
+  weightByJin: state => {
+    if (!state.globalWeight) {
+      return state.globalWeight
+    } else {
+      return parseInt(state.globalWeight) * 2
+    }
+  },
+  bodyMeasureIndexByGetter: (state, getters) => {
+    return state.globalWeight / (getters.heightByMeter ** 2)
+  }
+}
+
 const mutations = {
+  SET_GLOBAL_HEIGHT (state, payload) {
+    state.globalHeight = payload.height
+  },
+  SET_GLOBAL_WEIGHT (state, payload) {
+    state.globalWeight = payload.weight
+  },
+  SET_GLOBAL_COUNTRY (state, payload) {
+    state.globalCountry = payload.country
+  },
+  SET_GLOBAL_PROVINCE (state, payload) {
+    state.globalProvince = payload.province
+  },
+  SET_GLOBAL_WORK_PROVINCE (state, payload) {
+    state.globalWorkProvince = payload.workProvince
+  }
 }
 
 const actions = {
+  changeGlobalProvince (context, payload) {
+    setTimeout(() => {
+      context.commit('SET_GLOBAL_PROVINCE', payload)
+    }, 1500)
+  },
+  changeGlobalWorkProvince ({ commit, dispatch, getters, state, rootGetters, rootState }, payload) {
+    setTimeout(() => {
+      commit('SET_GLOBAL_WORK_PROVINCE', payload)
+    }, 2000)
+  }
 }
 
 export default {
   namespaced: true,
   state,
+  getters,
   mutations,
   actions
 }
--- src/views/vuex-practice/index.vue
+++ src/views/vuex-practice/index.vue
@@ -34,6 +34,7 @@
           <el-button
             slot="append"
             icon="el-icon-refresh"
+            @click="refreshHeight"
           >refreshHeight</el-button>
         </el-input>
       </el-col>
@@ -47,11 +48,36 @@
           <el-button
             slot="append"
             icon="el-icon-refresh"
+            @click="refreshWeight"
           >refreshWeight</el-button>
         </el-input>
       </el-col>
       <el-col :span="12">
         <p class="content">{{ globalWeightWithDifferentKeyName }}</p>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <p class="content">Height(m): </p>
+      </el-col>
+      <el-col :span="12">
+        <p class="content">{{ heightByMeter }}</p>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <p class="content">Weight(斤): </p>
+      </el-col>
+      <el-col :span="12">
+        <p class="content">{{ weightByJin }}</p>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <p class="content">BMI(By getter): </p>
+      </el-col>
+      <el-col :span="12">
+        <p class="content">{{ bodyMeasureIndexByGetter + '%' }}</p>
       </el-col>
     </el-row>
     <el-row>
@@ -64,7 +90,13 @@
     </el-row>
     <el-row>
       <el-col :span="12">
-        <p class="content">Country: </p>
+        <el-input v-model="country">
+          <el-button
+            slot="append"
+            icon="el-icon-refresh"
+            @click="refreshCountry"
+          >refreshCountry</el-button>
+        </el-input>
       </el-col>
       <el-col :span="12">
         <p class="content">{{ globalCountry }}</p>
@@ -72,7 +104,13 @@
     </el-row>
     <el-row>
       <el-col :span="12">
-        <p class="content">Province: </p>
+        <el-input v-model="province">
+          <el-button
+            slot="append"
+            icon="el-icon-refresh"
+            @click="refreshProvince"
+          >refreshProvince</el-button>
+        </el-input>
       </el-col>
       <el-col :span="12">
         <p class="content">{{ globalProvince }}</p>
@@ -80,7 +118,13 @@
     </el-row>
     <el-row>
       <el-col :span="12">
-        <p class="content">workProvince: </p>
+        <el-input v-model="workProvince">
+          <el-button
+            slot="append"
+            icon="el-icon-refresh"
+            @click="refreshWorkProvince"
+          >refreshWorkProvince</el-button>
+        </el-input>
       </el-col>
       <el-col :span="12">
         <p class="content">{{ globalWorkProvince }}</p>
@@ -91,7 +135,7 @@
 
 <script>
 import store from '@/store'
-import { mapState } from 'vuex'
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'VuexPractice',
@@ -100,16 +144,15 @@ export default {
       name: 'nizhm',
       age: '24',
       height: '173',
-      weight: '60'
+      weight: '60',
+      country: 'China',
+      province: 'GuangXi',
+      workProvince: 'HuNan'
     }
   },
   computed: {
-    /**
-     * use store to retrieve state, however it causes the component to rely on the global store singleton
-     *
-     * @see https://v3.vuex.vuejs.org/guide/state.html#getting-vuex-state-into-vue-components
-     * @return {vuexPractice.globalName}
-     */
+    // use store to retrieve state, however it causes the component to rely on the global store singleton;
+    // @see https://v3.vuex.vuejs.org/guide/state.html#getting-vuex-state-into-vue-components
     globalName () {
       return store.state.vuexPractice.globalName
     },
@@ -134,11 +177,21 @@ export default {
     // access this object by using standard function;
     ...mapState({
       bodyMeasureIndex (state) {
-        return this.globalWeight / (state.vuexPractice.globalHeight / 100) ^ 2
+        return this.globalWeightWithDifferentKeyName / ((state.vuexPractice.globalHeight / 100) ** 2)
       }
-    })
+    }),
+    ...mapGetters('vuexPractice', ['heightByMeter', 'weightByJin', 'bodyMeasureIndexByGetter'])
   },
   methods: {
+    // map `this.SET_GLOBAL_WEIGHT(payload)` to `this.$store.commit('vuexPractice/SET_GLOBAL_WEIGHT', payload)`
+    // @see https://v3.vuex.vuejs.org/guide/mutations.html#committing-mutations-in-components
+    ...mapMutations('vuexPractice', ['SET_GLOBAL_WEIGHT']),
+    // map `this.updateCountry(payload)` to `this.$store.commit('vuexPractice/SET_GLOBAL_COUNTRY', payload)`
+    ...mapMutations('vuexPractice', {
+      updateCountry: 'SET_GLOBAL_COUNTRY'
+    }),
+    // map `this.changeGlobalWorkProvince(payload)` to `this.$store.dispatch('vuexPractice/changeGlobalWorkProvince', payload)`
+    ...mapActions('vuexPractice', ['changeGlobalWorkProvince']),
     refreshName () {
       // do not recommend, instead using Mutations and Actions;
       store.state.vuexPractice.globalName = this.name
@@ -146,6 +199,23 @@ export default {
     refreshAge () {
       // do not recommend, instead using Mutations and Actions;
       this.$store.state.vuexPractice.globalAge = this.age
+    },
+    refreshHeight () {
+      // commit mutation directly in non-asynchronous operation;
+      this.$store.commit('vuexPractice/SET_GLOBAL_HEIGHT', { height: this.height })
+    },
+    refreshWeight () {
+      this.SET_GLOBAL_WEIGHT({ weight: this.weight })
+    },
+    refreshCountry () {
+      this.updateCountry({ country: this.country })
+    },
+    refreshProvince () {
+      // dispatch action instead of mutation with asynchronous operation;
+      this.$store.dispatch('vuexPractice/changeGlobalProvince', { province: this.province })
+    },
+    refreshWorkProvince () {
+      this.changeGlobalWorkProvince({ workProvince: this.workProvince })
     }
   }
 }
